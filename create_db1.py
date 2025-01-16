7d58904a5abc5f4ef9b05b0f9df7d4e20a7741de
catia91100 <catia.catarino@yahoo.fr>
2025-01-16 16:31:05 +0100

    Update create_db1.py

--- create_db1.py
+++ create_db1.py
@@ -1,6 +1,6 @@
 import sqlite3
 
-connection = sqlite3.connect('database.db1')
+connection = sqlite3.connect('database1.db')
 
 with open('schema1.sql') as f:
     connection.executescript(f.read())
