a0498b3a18b80d93ed7007f526bc5cfe388239d7
catia91100 <catia.catarino@yahoo.fr>
2025-01-16 15:06:44 +0100

    Update schema1.sql

--- schema1.sql
+++ schema1.sql
@@ -4,7 +4,7 @@ id INTEGER PRIMARY KEY AUTOINCREMENT,
 created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 nom  TEXT NOT NULL,
 prenom TEXT NOT NULL,
-adresse TEXT NOT NULL,
+adresse TEXT NOT NULL
 );
   
 DROP TABLE IF EXISTS livres;
@@ -16,16 +16,16 @@ auteur TEXT NOT NULL,
 genre TEXT NOT NULL,
 annee_publication TEXT NOT NULL,
 stock_total (NULL pas de livre enregistré),
-stock_disponible (NULL aucun livre disponible),
+stock_disponible (NULL aucun livre disponible)
   );
   
 DROP TABLE IF EXISTS emprunts;
 CREATE TABLE emprunts (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-id_utilisateurs 
-id_livres
-date_emprunt
+id_utilisateurs, 
+id_livres,
+date_emprunt,
 date_retour
 );
 
