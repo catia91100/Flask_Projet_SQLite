a4bdcd8d4ab8cee86bb85c86d041710336492c3c
catia91100 <catia.catarino@yahoo.fr>
2025-01-16 16:16:32 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -70,7 +70,7 @@ def livres():
     cursor.execute('SELECT * FROM livres;')
     data = cursor.fetchall()
     conn.close()
-    return render_template('livres.html')
+    return render_template('livres.html', data=data)
 """
 @app.route('/utilisateurs')
 def utilisateurs():
