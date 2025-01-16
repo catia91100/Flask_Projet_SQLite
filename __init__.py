300c55f14891a3b2d497142482cb00f309ba8d84
catia91100 <catia.catarino@yahoo.fr>
2025-01-16 12:28:44 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -49,11 +49,21 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
     
-@app.route('/fiche_nom/')
-def fiche_nom():
+@app.route('/fiche_nom/<post_nom>')
+def fiche_nom(post_nom):
     if not est_authentifie():
         # Rediriger vers la page d'authentification si l'utilisateur n'est pas authentifié
         return redirect(url_for('authentification'))
+    else:
+            conn = sqlite3.connect('database.db')
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_nom,))
+            data = cursor.fetchall()
+            conn.close()
+            # Rendre le template HTML et transmettre les données
+            return render_template('read_data.html', data=data)
+
+
     
     
 @app.route('/consultation/')
