78c38352d1cd7dde879779513088d203d9096208
catia91100 <catia.catarino@yahoo.fr>
2025-01-16 14:56:43 +0100

    Update create_db1.py

--- create_db1.py
+++ create_db1.py
@@ -13,9 +13,9 @@ cur.execute("INSERT INTO utilisateurs (nom, prenom, adresse) VALUES (?, ?, ?)",(
 cur.execute("INSERT INTO livres (titre, auteur, genre, annee_publication, stock_total) VALUES (?, ?, ?, ?, ?)",('Empire des anges', 'WEBER', 'Roman', '2000', '20'))
 cur.execute("INSERT INTO livres (titre, auteur, genre, annee_publication, stock_total) VALUES (?, ?, ?, ?, ?)",('LE HORLA', 'MAUPASSANT', 'Nouvelle Fantastique', '1887', '10'))
 cur.execute("INSERT INTO livres (titre, auteur, genre, annee_publication, stock_total) VALUES (?, ?, ?, ?, ?)",('Le Bourgeois Gentilhomme', 'MOLIERE', 'COMEDIE', '1670', '15'))
-cur.execute("INSERT INTO emprunts (id_clients, id_livres, date_emprunt, date_retour) VALUES (?, ?, ?, ?)",('1', '3', '01/01/2024, '13/01/2024'))
-cur.execute("INSERT INTO emprunts (id_clients, id_livres, date_emprunt, date_retour) VALUES (?, ?, ?, ?)",('2', '1', '04/01/2024, '14/01/2024'))
-cur.execute("INSERT INTO emprunts (id_clients, id_livres, date_emprunt, date_retour) VALUES (?, ?, ?, ?)",('3', '2', '04/01/2024, '14/01/2024'))
+cur.execute("INSERT INTO emprunts (id_clients, id_livres, date_emprunt, date_retour) VALUES (?, ?, ?, ?)",('1', '3', '01/01/2024', '13/01/2024'))
+cur.execute("INSERT INTO emprunts (id_clients, id_livres, date_emprunt, date_retour) VALUES (?, ?, ?, ?)",('2', '1', '04/01/2024', '14/01/2024'))
+cur.execute("INSERT INTO emprunts (id_clients, id_livres, date_emprunt, date_retour) VALUES (?, ?, ?, ?)",('3', '2', '04/01/2024', '14/01/2024'))
 connection.commit()
 connection.close()
 
