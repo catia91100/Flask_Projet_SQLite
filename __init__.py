775a3ea18dd67605fa06e602cc832de4fc579e04
catia91100 <catia.catarino@yahoo.fr>
2025-01-16 11:23:32 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -63,6 +63,25 @@ def Readfiche(post_id):
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
 
+@app.route('/fiche_nom/<int:post_nom>')
+def Readfiche(post_nom):
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM clients WHERE nom = ?', (post_nom,))
+    data = cursor.fetchall()
+    conn.close()
+     if request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
+            session['authentifie'] = True
+            # Rediriger vers la route lecture après une authentification réussie
+            # Rendre le template HTML et transmettre les données
+                return render_template('read_data.html', data=data)  
+        else:
+            # Afficher un message d'erreur si les identifiants sont incorrects
+            return render_template('formulaire_authentification.html', error=True)
+        return render_template('formulaire_authentification.html', error=False)
+ 
+    
+
 @app.route('/consultation/')
 def ReadBDD():
     conn = sqlite3.connect('database.db')
