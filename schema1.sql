a62bde3298e34fea5541a666a3b5a2219d6844a3
catia91100 <catia.catarino@yahoo.fr>
2025-01-16 13:47:03 +0100

    Update schema1.sql

--- schema1.sql
+++ schema1.sql
@@ -2,38 +2,35 @@ DROP TABLE IF EXISTS utilisateurs;
 CREATE TABLE utilisateurs (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-- id_user (PK)
-- nom
-- prenom
-- email (unique)
-- mot_de_passe
-- role (Admin ou Utilisateur)
-  )
+- nom  TEXT NOT NULL,
+- prenom TEXT NOT NULL,
+- email TEXT NOT NULL,
+- mot_de_passe TEXT NOT NULL,
+  );
   
 DROP TABLE IF EXISTS livres;
 CREATE TABLE livres (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-- id_livre (PK)
-- titre
-- auteur
-- genre
-- annee_publication
-- stock_total
-- stock_disponible
-  )
+- titre TEXT NOT NULL,
+- auteur TEXT NOT NULL,
+- genre TEXT NOT NULL,
+- annee_publication TEXT NOT NULL,
+- stock_total (NULL pas de livre enregistré),
+- stock_disponible (NULL aucun livre disponible),
+  );
   
-DROP TABLE IF EXISTS Emprunts;
-CREATE TABLE Emprunts (
+DROP TABLE IF EXISTS emprunts;
+CREATE TABLE emprunts (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-- id_emprunt (PK)
+- id_emprunts (PK)
 - id_user (FK, Utilisateurs.id_user)
 - id_livre (FK, Livres.id_livre)
 - date_emprunt
 - date_retour_prevue
 - date_retour_effective (NULL si pas encore retourné)
-  )
+  );
 
 
 
